ef47a22f7af59104804cc68539b42dbd91b2d2c0
leon-chiang-ics-4u1-02-26 <Leon.Chiang@student.tdsb.on.ca>
2026-03-03 15:04:19 +0000

    Solution

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,34 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean validIndex(int[] x, int y) {
+		try {
+			x[y]=0;
+		}
+		catch (ArrayIndexOutOfBoundsException aIE) {
+			return false;
+		}
+		return true;
+	}
+
+	public static int divide(int x, int y) {
+		try {
+			return x/y;
+		}
+		catch (ArithmeticException aE) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String x) {
+		try {
+			return Integer.parseInt(x);
+		}
+		catch (NumberFormatException nFE) {
+			return 0;
+		}
+	}
+
 	
 
 }
